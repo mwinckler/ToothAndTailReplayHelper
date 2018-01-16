29a5ad807f8b001d61735eb185228cdb0ea820ae
Matt Winckler <himself@mattwinckler.com>
2018-01-15 22:13:37 -0800

    Add date format help link

---
+++ ToothAndTailReplayHelper/Command/OpenDateFormatHelpCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ToothAndTailReplayHelper.Command
+{
+    internal sealed class OpenDateFormatHelpCommand : IOpenDateFormatHelpCommand
+    {
+        private const string HelpUri = @"https://docs.microsoft.com/en-us/dotnet/standard/base-types/custom-date-and-time-format-strings";
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter)
+        {
+            Process.Start(HelpUri);
+        }
+    }
+}
